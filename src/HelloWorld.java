74fdee1c743fa0214a1875611f1ffe59d48c5a3b
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-12-14 15:38:24 -0600

    java syntax, types and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,119 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.print("Hello");
         System.out.print(" World!");
+
+        byte month = 12;
+        System.out.println(month);
+
+        short daysInYear = 365;
+        System.out.println(daysInYear);
+
+        String greeting = "Hello, there!";
+        System.out.println(greeting);
+
+        String name;
+        name = "Alfonso";
+        System.out.println(name);
+
+        float pi = 3.14f;
+        System.out.println(pi);
+
+        // cannot reassign the value of a final variable.
+        final String APIKEY = "asdfasdf34234lkj234k23l";
+        System.out.println(APIKEY);
+
+
+        int firstNumber = 20;
+        int secondNumber = 25;
+        System.out.println(firstNumber + secondNumber);
+
+        int increment = 0;
+        increment++;
+        System.out.println(increment);
+
+        int decrement = increment;
+
+        decrement--;
+
+        System.out.println(decrement);
+
+
+
+        // Implicit CASTING - less specific data type, to a more specific data type.
+
+        byte dayOfMonth = 14;
+        byte birthday = 24;
+
+        int result = dayOfMonth * birthday;
+
+
+
+        // Explicit Casting - more specific data type, to less specific data type.
+//        int x = 900;
+//        int y = 800;
+//
+//        int difference = (byte)(x -y);
+//
+//        System.out.println(difference);
+
+        int myFavoriteNumber = 2;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Pearl Jam";
+        System.out.println(myString);
+
+        // question 3: notice error
+        // char tempString = myString;
+
+        // question 4: notice error - data type
+        // myString = 3.14159;
+
+        // question 5: error message received - variable not initialized
+        // question 6: error due to length of number, could be an int or short
+        // question 8: due to length of number
+        // question 9: change to double or cast float myNumber = (float)(3.14);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        // question 10 pre and post increment
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        // int x = 5;
+        // System.out.println(++x);
+        // System.out.println(x);
+
+        // question 11: error received - class is a reserved word
+        // int class;
+
+        // question 12: error received String and Interger incompatible
+        // String theNumberThree = "three";
+        // Object o = theNumberThree;
+        // int three = (int) o;
+
+        // string cannot be converted to int
+        // int three = (int) "three";
+
+
+        // question 13
+        // int x = 4;
+        // x += 5;
+
+        // int x = 3;
+        // int y = 4;
+        // y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /=y;
+        y -=x;
+
+        // question 14 : you must use implicit or explicit casting to resolve the issue.
+
+
+
+
+
     }
 }
