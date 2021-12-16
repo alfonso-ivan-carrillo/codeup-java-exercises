e87d89356a7ac0c3b7524eaac9d8152e7ae112a3
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-12-16 16:35:27 -0600

    completed third question

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main (String[] args){
 
@@ -8,11 +10,11 @@ public class ControlFlowExercises {
 //            System.out.print(i);
 //            i++;
 //        }
-
-        for (int i = 5; i <= 15; i++){
-            System.out.print(i);
-        }
-        System.out.printf("%n");;
+//
+//        for (int i = 5; i <= 15; i++){
+//            System.out.print(i);
+//        }
+//        System.out.printf("%n");;
 
         // count by 2
 //        int x = 0;
@@ -37,30 +39,65 @@ public class ControlFlowExercises {
 //            System.out.println(x);
 //            x = x * x;
 //        } while (x < 100000);
+//
+//        for (long y = 2; y <= 100; y +=2){
+//            System.out.println(y);
+//        }
+//
+//        for (long y = 100; y >= -10; y -=5){
+//            System.out.println(y);
+//        }
+//
+//        for (long y = 2; y <= 1000000; y = y * y){
+//            System.out.println(y);
+//        }
+//
+//        for (int x = 0; x <= 100; x++){
+//            if (x % 3 == 0){
+//                System.out.println("Fizz");
+//            } else if (x % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if ( x % 3 == 0 && x % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            } else {
+//                System.out.println(x);
+//            }
+//        }
 
-        for (long y = 2; y <= 100; y +=2){
-            System.out.println(y);
-        }
+        Scanner scn = new Scanner(System.in);
+        String choice = "y";
 
-        for (long y = 100; y >= -10; y -=5){
-            System.out.println(y);
-        }
+        while (choice.equalsIgnoreCase("y")){
+            System.out.println("What number would you like to go up to?");
+            int userInput = scn.nextInt();
+            System.out.println("Number | " + "Squared |" + " Cubed");
+            System.out.println("_______| " + "________|" + "_______");
+//            System.out.println("       |" + "         |" + "      ");
 
-        for (long y = 2; y <= 1000000; y = y * y){
-            System.out.println(y);
-        }
+            for (int i = 1; i <= userInput; i++){
+                int numberSquared = (int) Math.pow(i, 2);
+                int numberCubed = (int) Math.pow(i, 3);
+                String message ="\n" + i + "      | " + numberSquared + "       | " + numberCubed;
 
-        for (int x = 0; x <= 100; x++){
-            if (x % 3 == 0){
-                System.out.println("Fizz");
-            } else if (x % 5 == 0){
-                System.out.println("Buzz");
-            } else if ( x % 3 == 0 && x % 5 == 0){
-                System.out.println("FizzBuzz");
-            } else {
-                System.out.println(x);
+                System.out.println(message);
+
+
             }
+            System.out.print("Continue? (y/n): " );
+            choice = scn.next();
+            System.out.println();
         }
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
