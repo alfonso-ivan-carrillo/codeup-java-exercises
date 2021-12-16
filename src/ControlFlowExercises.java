ae623b66b29502b2a97e7eb48b15d9f160d2e84a
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-12-16 15:02:37 -0600

    completing exercise and refactor

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,54 @@
 public class ControlFlowExercises {
     public static void main (String[] args){
-        System.out.println("hello");
 
-        int i = 5;
 
-        while (i <= 15){
+//        int i = 5;
+//
+//        while (i <= 15){
+//            System.out.print(i);
+//            i++;
+//        }
+
+        for (int i = 5; i <= 15; i++){
             System.out.print(i);
-            i++;
+        }
+        System.out.printf("%n");;
+
+        // count by 2
+//        int x = 0;
+//
+//        do {
+//            x += 2;
+//            System.out.println(x);
+//        } while( x < 100);
+
+        // count by 5
+//        int x = 100;
+//        do {
+//            x -=5;
+//            System.out.println(x);
+//        } while (x > -10);
+
+        // squared
+
+//        long x = 2;
+
+//        do {
+//            System.out.println(x);
+//            x = x * x;
+//        } while (x < 100000);
+
+        for (long y = 2; y <= 100; y +=2){
+            System.out.println(y);
         }
 
+        for (long y = 100; y >= -10; y -=5){
+            System.out.println(y);
+        }
+
+        for (long y = 2; y <= 1000000; y = y * y){
+            System.out.println(y);
+        }
 
 
 
