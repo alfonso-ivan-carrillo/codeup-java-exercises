448631593256b34eb341e4062b6d4c4613257750
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-03 13:39:37 -0600

    completed java string exercise question 2

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+
+        Scanner scn = new Scanner(System.in);
+        String askBobAnotherQuestion = "yes";
+        System.out.println("Ask Bob a question.");
+
+        while (askBobAnotherQuestion.equalsIgnoreCase("yes")){
+            String userInput = scn.nextLine();
+            if (userInput.endsWith("?")){
+                System.out.println("Bob: Sure.");
+            } else if (userInput.endsWith("!")){
+                System.out.println("Bob: Whoa, chill out!");
+            } else if (userInput.equals(" ")){
+                System.out.println("Bob: Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            System.out.println("Ask Bob another question? (yes/no)");
+            askBobAnotherQuestion = scn.nextLine();
+
+        }
+
+    }
+}
