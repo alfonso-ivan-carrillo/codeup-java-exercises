7c6baa7bba768456021ed75b71d6536ca375cd77
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-04 10:31:47 -0600

    completed question 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
-
+    // Start of Question 1
     // addition method
     public static int additionMethod(int num1, int num2){
         return num1 + num2;
@@ -40,6 +42,31 @@ public class MethodsExercises {
         }
         return num3;
     }
+    // End of Question 1
+
+    // Question 2
+
+    public static int getInteger(int min, int max){
+//        System.out.println("Enter a number between 1 and 20:");
+//        Scanner scn = new Scanner(System.in);
+//        int userInput1 = scn.nextInt();
+//        min = userInput1;
+//        System.out.println("Enter a second number between 1 and 20:");
+//        int userInput2 = scn.nextInt();
+//        max = userInput2;
+        if (min >= 0 && min <= 20 && max >= 0 && max <= 20){
+            System.out.println("Your range is: " + (max - min));
+            return min - max;
+        } else {
+            System.out.println("Numbers not in range.");
+            System.out.println("Enter a number between 1 and 20:");
+            Scanner scn = new Scanner(System.in);
+            int userInput1 = scn.nextInt();
+            System.out.println("Enter a second number between 1 and 20:");
+            int userInput2 = scn.nextInt();
+            return getInteger(userInput1, userInput2);
+        }
+    }
 
     public static void main(String[] args){
 
@@ -50,7 +77,15 @@ public class MethodsExercises {
 ////        System.out.println(divisionMethod(10, 0));
 //        System.out.println(modulusMethod(10, 2));
 //        System.out.println(modulusMethod(10, 3));
-        System.out.println(multiMethod(3, 3));
+//        System.out.println(multiMethod(3, 3));
+
+        System.out.println("Enter a number between 1 and 20:");
+        Scanner scn = new Scanner(System.in);
+        int userInput1 = scn.nextInt();
+        System.out.println("Enter a second number between 1 and 20:");
+        int userInput2 = scn.nextInt();
+        getInteger(userInput1, userInput2);
+
 
     }
 
