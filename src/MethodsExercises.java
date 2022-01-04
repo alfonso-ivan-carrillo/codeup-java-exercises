a33126f5c7e477f4a79e2e9085b41be48effa546
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-04 09:07:39 -0600

    warm up exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,56 @@
+public class MethodsExercises {
+
+
+    // addition method
+    public static int additionMethod(int num1, int num2){
+        return num1 + num2;
+    }
+
+    // subtraction method
+    public static int subtractionMethod(int num1, int num2){
+        return num1 - num2;
+    }
+
+    // multiplication method
+    public static int multiplicationMethod(int num1, int num2){
+        return num1 * num2;
+    }
+
+
+    // division method
+    public static int divisionMethod(int num1, int num2){
+        if(num1 == 0){
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    // modulus method
+    public static int modulusMethod(int num1, int num2){
+        return num1 % num2;
+    }
+
+    // Bonus for Question 1
+    public static int multiMethod(int num1, int num2){
+        int counter = 1;
+        while (counter <= num2){
+            num1 += num1;
+            counter++;
+        }
+        return num1;
+    }
+
+    public static void main(String[] args){
+
+//        System.out.println(additionMethod(5, 10));
+//        System.out.println(subtractionMethod(9, 19));
+//        System.out.println(multiplicationMethod(5, 5));
+//        System.out.println(divisionMethod(10, 2));
+////        System.out.println(divisionMethod(10, 0));
+//        System.out.println(modulusMethod(10, 2));
+//        System.out.println(modulusMethod(10, 3));
+        System.out.println(multiMethod(2, 4));
+
+    }
+
+}
--- src/Warmups.java
+++ src/Warmups.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Warmups {
+
+    public static String startDay(){
+        return "Brain loading... Done!";
+    }
+
+
     public static void main (String[] args){
 
         String message = "Java is statically typed, while Javascript is dynamically typed.";
@@ -13,7 +19,9 @@ public class Warmups {
         String breakfast = scannerBreakfast.nextLine();
 
         System.out.println(breakfast + " are awesome!" );
-        System.out.printf("%s, I wish i had that for brekfast!", breakfast);
+        System.out.printf("%s, I wish i had that for brekfast!\n", breakfast);
+
+        System.out.println(startDay());
 
 
     }
